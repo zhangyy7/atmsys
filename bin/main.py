b1718266d9fee1c9c5966e703c9ae0cc587960ba
zhangyy <m_zhangyinyue@163.com>
2016-11-28 17:19:33 +0800

    mm

--- bin/main.py
+++ bin/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+import sys
 from bin import credit as ct
 from bin import shopping as sp
 from modules.credit import system
@@ -9,7 +10,8 @@ from conf import templates as tl
 def main():
     menu = {
         "1": {"module": ct, "func": "main"},
-        "2": {"module": sp, "func": "main"}
+        "2": {"module": sp, "func": "main"},
+        "3": {"module": sys, "func": "exit"}
     }
     system.charge_out()
     c_menu = input(tl.format_page(tl.INDEX_MEMU))
